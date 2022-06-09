0f6a77748d5af9aadabe57362d6941cc7d2cc7d2
FaranushKarimov <faranush.karimov@gmail.com>
2022-06-09 18:17:31 +0300

    Automate Migration

--- Darmon Tajkhizot/Program.cs
+++ Darmon Tajkhizot/Program.cs
@@ -1,4 +1,7 @@
+using Entities.DataContexts;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 namespace Darmon_Tajkhizot
 {
@@ -6,7 +9,14 @@ namespace Darmon_Tajkhizot
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+                db.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
