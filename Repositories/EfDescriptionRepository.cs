129d86f84d8508edb5028988f1978dc8e4415659
FaranushKarimov <faranush.karimov@gmail.com>
2022-06-16 11:56:34 +0300

    Testing

--- Repositories/EfDescriptionRepository.cs
+++ Repositories/EfDescriptionRepository.cs
@@ -17,7 +17,13 @@ namespace Repositories
         {
 
         }
-
+        public async Task CreateDescription(Description description) => await CreateAsync(description);
+        public async Task<IEnumerable<DescriptionResponse>> GetAllAsync(bool trackChanges) => await FindAll(trackChanges).Select(x => new DescriptionResponse
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Text = x.Text
+        }).ToListAsync();
         public async Task<Description> GetByIdAsync(Guid id, bool trackChanges) => await FindByCondition(x => x.Id.Equals(id), trackChanges).FirstOrDefaultAsync();
 
 
---
+++ Tests/DescriptionsServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using Entities.DataContexts;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Repositories;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class DescriptionsServiceTest
+    {
+        private static DbContextOptions<DataContext> dbContextOptions = new DbContextOptionsBuilder<DataContext>().UseInMemoryDatabase(databaseName: "Darmon").Options;
+        DataContext context;
+        DescriptionService descriptionService;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            context = new DataContext(dbContextOptions);
+            context.Database.EnsureCreated();
+            SeedDatabase();
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            context.Database.EnsureDeleted();
+        }
+
+        private void SeedDatabase()
+        {
+            var descriptions = new List<Description>
+            {
+               new Description()
+               {
+                   Id = Guid.NewGuid(),
+                   Name = "Name1",
+                   Text = "Text1"
+               },
+               new Description()
+               {
+                   Id = Guid.NewGuid(),
+                   Name = "Name2",
+                   Text = "Text2"
+               }
+            };
+            context.Descriptions.AddRange(descriptions);
+            
+            //var categories = new List<Category>
+            //{
+            //    new Category()
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        ImagePath = @"C:\Users\Faramush\Downloads\photo_2022-05-28_11-29-58.jpg",
+
+            //    }
+
+            //};
+            
+        }
+
+
+    }
+}
